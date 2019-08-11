aee7e187fa53bbe1a1d8d0acdb6341b909d141c4
Phil Ashby <phil.github@ashbysoft.com>
2019-08-11 17:18:17 +0100

    Remove confluence bits

--- moin2markdown.sh
+++ moin2markdown.sh
@@ -3,6 +3,7 @@
 # change to directory where script is located
 CUR=$(dirname "$0")
 WORKING=$CUR/working
+[ -n "$TMP" ] && WORKING=$TMP/moin2markdown
 
 PAGENAME=$1
 MOINROOT=$2
@@ -40,35 +41,35 @@ sed -i -r 's/ ([/]?[A-Z][A-Za-z0-9]+[A-Z]+[a-z0-9]+)/ [[\1]]/g' "$WORKING/$PAGEN
 # remove ''''''
 sed -i "s/\([A-Z]([A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*\)/[[\1]]/g" "$WORKING/$PAGENAME.wiki"
 
-# Start table handling
+# Start table handling; PAA - omit for Markdown??
 # This table handling is a hack because we actually convert straight to confluence format and not mediawiki format
 
 # handle 8 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><td>\7<\/td><td>\8<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><td>\7<\/td><td>\8<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 7 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><td>\7<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><td>\7<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 6 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><td>\6<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 5 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><td>\5<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 4 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><td>\4<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 3 column tables
-sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # handle 2 column tables
-sed -i "s/^||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
+#sed -i "s/^||\(.*\)||\(.*\)||/<tr><td>\1<\/td><td>\2<\/td><\/tr>/g" "$WORKING/$PAGENAME.wiki"
 
 # add <table> to start of table
-perl -0777 -i -pe 's/[^>]\r\n<tr>/\r\n<table>\r\n<tr>/g' "$WORKING/$PAGENAME.wiki"
+#perl -0777 -i -pe 's/[^>]\r\n<tr>/\r\n<table>\r\n<tr>/g' "$WORKING/$PAGENAME.wiki"
 
 # add </table> to end of table
-perl -0777 -i -pe 's/<\/tr>\r\n[^<]/<\/tr>\r\n<\/table>/g' "$WORKING/$PAGENAME.wiki"
+#perl -0777 -i -pe 's/<\/tr>\r\n[^<]/<\/tr>\r\n<\/table>/g' "$WORKING/$PAGENAME.wiki"
 
 # end table handling
 
@@ -91,15 +92,6 @@ CLEANFILE=`echo $CLEANFILE | sed "s/(2f)/__/g"`
 # -
 CLEANFILE=`echo $CLEANFILE | sed "s/(2d)/'/g"`
 
-# converts markdown to confluence
-pandoc "$WORKING/$PAGENAME.md" -t confluence.lua > "$OUTPUT/$CLEANFILE"
-
-# Here we replace the subpage links with Page__ChildPage so that the links still work
-
-# convert subpage link 1
-sed -i "s/href='\/\(.*\)'/href='${CLEANFILE}__\1'/g" "$OUTPUT/$CLEANFILE"
-
-# convert subpage link 2
-sed -i "s/href='${CLEANFILE}\/\(.*\)'/href='${CLEANFILE}__\1'/g" "$OUTPUT/$CLEANFILE"
-
-
+echo "$PAGENAME > $CLEANFILE"
+mv "$WORKING/$PAGENAME.md" "$OUTPUT/$CLEANFILE.md"
+rm -rf $WORKING
